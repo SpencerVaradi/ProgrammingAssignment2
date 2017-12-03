7450e9f8ad9bd66191b2b2b5081cb78761fb2d3c
Spencer Varadi <spencervaradi@gmail.com>
2017-12-03 12:01:25 -0500

    Functions written with example

--- cachematrix.R
+++ cachematrix.R
@@ -2,31 +2,41 @@
 ## functions do
 #These functions cache the inverse of a provided matrix.
 ## Write a short comment describing this function
+
 #This function  creates a special object that is the inverse of the matrix
 #supplied. It sets the matrix, gets it, and sets functions to solve
 makeCacheMatrix <- function(x = matrix()) {
-        s <- NULL
-        set <- function(y){
-                x <<- y
-                s <<- NULL
-        }
-        get <- function() x
-        setsolve <- function(solve) s <<- solve
-        getsolve <- function() s
-        list(set=set, get=get,
-             setsolve = setsolve,
-             getsolve = getsolve)
+  # Scrub the solved matrix
+  solved <- NULL
+  # Function to change the target matrix
+  set <- function(newSet) {
+    x <<- newSet
+    solved <<- NULL
+  }
+
+  # Function to retrieve the stored matrix
+  get <- function() x
+  # Function to invert supplied matrix
+  setSolve <- function(solve) solved <<- solve
+  # Function get inverted matrix
+  getSolve <- function() solved
+  # Retun list of functions
+  list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
 }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        s <- x$getsolve()
-        if(!is.null(s)){
-                message("getting cached data")
-                return(s)
-        }
-        data <- x$get()
-        s <- solve(data, ...)
-        x$setsolve(s)
-        s
+cacheSolve <- function(x) {
+  # Grab the cached inverted matrix
+  solved <- x$getSolve()
+  # If cached, returns that inverted matrix and exits
+  if (!is.null(solved)) {
+    return(solved)
+  }
+
+  # Does inversion instead
+  solve(x$get())
 }
+
+# Example
+inputMatrix <- matrix(seq(1,4),nrow = 2, ncol = 2)
+cachedMatrix <- makeCacheMatrix(inputMatrix)
+cacheSolve(cachedMatrix)
